f4db9f9d58de7de1b661f38c89f2a3b9958b947b
Nicolas <enbies00@gmail.com>
2022-12-17 08:22:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
 import './App.css';
 
 function App() {
+  const API_URL = "https://api.themoviedb.org/3/movie/popular?page=1&api_key=8f11a2c440e8e722cffbc76df58bc14c"
+
+  const API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=8f11a2c440e8e722cffbc76df58bc14c&query="
+
+  const [Movies, setMovies] = useState([])
+  const [value, setValue] = useState('')
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => setMovies(data.results))
+  }, [])
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+
+    fetch(API_SEARCH + value)
+      .then(response => response.json())
+      .then(data => setMovies(data.results))
+  }
+
+  console.log(Movies);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Enbies Movie</h1>
+      <nav>
+        <form onSubmit={handleSearch}>
+          <input type="text"
+            placeholder='Search Movie here'
+            onChange={(e) => setValue(e.target.value)} />
+          <button>Search</button>
+        </form>
+      </nav>
+      <main>
+        {Movies.map((movie) => <MovieCard {...movie} key={movie.id} />)}
+      </main>
+
+    </>
   );
 }
 
