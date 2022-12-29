fb7d888c66ebd7c088552bd88e07a0f445fc90fc
Nicolas <enbies00@gmail.com>
2022-12-29 15:46:14 +0700

    master to main

---
+++ src/MovieCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const MovieCard = (props) => {
+  const apiImg = "https://image.tmdb.org/t/p/w500/"
+
+  return (
+    <div className="movieCard">
+      <div className="movieHeader">
+        <strong className='title'>{props.title}</strong>
+      </div>
+      <div className="poster">
+        <img src={props.poster_path == null ? "https://source.unsplash.com/320x400?movie-poster" : apiImg + props.poster_path} alt="poster" />
+      </div>
+      <div className='movieFooter'>
+        <span className='date'>{props.release_date}</span>
+        <span className='rate'>{props.vote_average}</span>
+      </div>
+    </div>
+  )
+}
+
+// 
+
+export default MovieCard
